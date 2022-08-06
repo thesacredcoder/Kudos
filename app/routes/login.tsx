2d8623cabd25420549afd368e89625b79ffe73c7
thesacredcoder <akashsamal4875@gmail.com>
2022-08-06 20:09:08 +0530

    built the user authentication routes

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,194 @@
-import { Layout } from "~/components";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+
+import { login, register, getUser } from "~/utils/auth.server";
+import { Layout, FormField } from "~/components";
+import {
+  validateEmail,
+  validateName,
+  validatePassword,
+} from "~/utils/validators.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return (await getUser(request)) ? redirect("/") : null;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const action = form.get("_action");
+  const email = form.get("email");
+  const password = form.get("password");
+  let firstName = form.get("firstName");
+  let lastName = form.get("lastName");
+
+  if (
+    typeof action !== "string" ||
+    typeof email !== "string" ||
+    typeof password !== "string"
+  ) {
+    return json({ error: `Invalid form data`, form: action }, { status: 400 });
+  }
+
+  if (
+    action === "register" &&
+    (typeof firstName !== "string" || typeof lastName != "string")
+  ) {
+    return json({ error: `Invalid form data`, form: action }, { status: 400 });
+  }
+
+  const errors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+    ...(action === "register"
+      ? {
+          firstName: validateName((firstName as string) || ""),
+          lastName: validateName((lastName as string) || ""),
+        }
+      : {}),
+  };
+
+  if (Object.values(errors).some(Boolean)) {
+    return json(
+      {
+        errors,
+        fields: { email, password, lastName, firstName },
+        form: action,
+      },
+      { status: 400 }
+    );
+  }
+
+  switch (action) {
+    case "login":
+      return await login({ email, password });
+    case "register":
+      firstName = firstName as string;
+      lastName = lastName as string;
+      return await register({ firstName, lastName, email, password });
+    default:
+      return json({ error: `Invalid Form Data` }, { status: 400 });
+  }
+};
 
 export default function Login() {
-  return <Layout>This is the login page</Layout>;
+  const actionData = useActionData();
+  const [formError, setFormError] = useState(actionData?.error || "");
+  const [errors, setErrors] = useState(actionData?.errors || {});
+  const firstLoad = useRef(true);
+  const [action, setAction] = useState("login");
+  const [formData, setFormData] = useState({
+    email: actionData?.fields?.email || "",
+    password: actionData?.fields?.password || "",
+    firstName: actionData?.fields?.firstName || "",
+    lastName: actionData?.fields?.lastName || "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData({ ...formData, [field]: event.target.value });
+  };
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      const newState = {
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+      };
+      setErrors(newState);
+      setFormError("");
+      setFormData(newState);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    if (!firstLoad.current) {
+      setFormError("");
+    }
+  }, [formData]);
+
+  useEffect(() => {
+    firstLoad.current = false;
+  });
+  return (
+    <Layout>
+      <div className="h-full flex justify-center items-center flex-col gap-y-4">
+        <button
+          onClick={() => setAction(action === "login" ? "register" : "login")}
+          className="absolute top-8 right-8 rounded-xl bg-yellow-300 text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          {action === "login" ? "Sign Up" : "Sign In"}
+        </button>
+        <h2 className="text-5xl font-extrabold text-yellow-300">
+          Welcome to Kudos!
+        </h2>
+        <p className="capitalize font-semibold text-slate-300">
+          {action === "login"
+            ? "Log In To give some praise!"
+            : "Sign Up To Get Started!"}
+        </p>
+
+        <form
+          method="post"
+          className="rounded-2xl bg-gray-200 p-6 w-96 shadow-md"
+        >
+          <div className="text-xs fonr-semibold text-center tracking-wide text-red-500 w-full">
+            {formError}
+          </div>
+          {action !== "login" && (
+            <>
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={(e) => handleInputChange(e, "firstName")}
+                error={errors?.firstName}
+              />
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={(e) => handleInputChange(e, "lastName")}
+                error={errors?.lastName}
+              />
+            </>
+          )}
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={(e) => handleInputChange(e, "email")}
+            error={errors?.email}
+          />
+          <FormField
+            htmlFor="password"
+            label="Password"
+            type="password"
+            value={formData.password}
+            onChange={(e) => handleInputChange(e, "password")}
+            error={errors?.password}
+          />
+          <div className="w-full text-center">
+            <button
+              type="submit"
+              name="_action"
+              value={action}
+              className="rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1 cursor-pointer shadow-sm"
+            >
+              {action === "login" ? "Sign In" : "Sign Up"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
 }
